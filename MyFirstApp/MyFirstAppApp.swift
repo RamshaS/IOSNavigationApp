4085e71017202ed3b81e8d8d958805764ad376eb
RamshaS <ramshasaeed2016@gmail.com>
2024-05-10 13:32:44 +0500

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by ramsha omer on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
